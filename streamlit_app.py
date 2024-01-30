99bfe8a65615aa8f3f89b6185cd4c3ee3fae04c8
MahaPeyar <141348388+MahaPeyar@users.noreply.github.com>
2024-01-30 22:49:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-my_cnx=snowflake.connector.connect(**strealit.secrets["snowflake"])
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 my_cur.execute("select current_user(), current_account(), current_region()")
 my_data_row=my_cur.fetchone()
