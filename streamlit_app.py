8f443051418696fe1f2296e14fe0ba009ab60dd8
MahaPeyar <141348388+MahaPeyar@users.noreply.github.com>
2024-01-30 12:45:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('Breakfast Menu')
 streamlit.title('Idly Dosa Sambar')
 streamlit.title('Chappathi Poori Tea')
 streamlit.header('Build your own smoothi')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
