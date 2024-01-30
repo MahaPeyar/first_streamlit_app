423d09960a1aefec2c8e584454ccaa6a9eb41f09
MahaPeyar <141348388+MahaPeyar@users.noreply.github.com>
2024-01-30 12:49:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ streamlit.header('Build your own smoothi')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+#lets put the fruits here. so that user can pickw hat they want to
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
