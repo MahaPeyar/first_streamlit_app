421e5a18ed0df41892dfff09f6b1f8e66f6df234
MahaPeyar <141348388+MahaPeyar@users.noreply.github.com>
2024-01-30 19:43:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
 streamlit.header('Fruityvice Fruit Advice!')
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+#import requests
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pandas.json.normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
