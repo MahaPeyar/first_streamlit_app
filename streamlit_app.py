2586be4f2d139ccbd0dbbf823b33c9b7e243fee5
MahaPeyar <141348388+MahaPeyar@users.noreply.github.com>
2024-02-02 17:12:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,10 @@ my_cur=my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row=my_cur.fetchone()
 streamlit.text("The current fruit load list contains")
-streamlit.text(my_data_row)    
+streamlit.text(my_data_row)   
+
+streamlit.header('Fruityvice Fruit Advice2!')
+fruit_choice=streamlit.text_input('What fruit whould you like to add?', 'jackfruit')
+streamlit.write('Thanks for adding ',fruit_choice)
+
 
